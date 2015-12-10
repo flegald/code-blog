2f770b5ba8fe03d60332280cf9ea59b6b8fff274
David Flegal <flegal.david@gmail.com>
2015-12-09 17:04:17 -0800

    fixed e tag sorting, finished admin stats page

---
+++ JS/admin_stats.js
@@ -0,0 +1,98 @@
+$(function(){
+var arrayLength = [];
+var realWordsArray = [];
+json_array = [];
+
+  function initArray(){
+    var json_array = [];
+    JSON.parse(localStorage.getItem('blogData')).forEach(function(article){
+      json_array.push(article);
+   });
+   return json_array;
+   };
+
+  function numOfArticles(data){
+    return data.length  };
+
+  var numOfAuths = function (data){
+     var authArray = [];
+     uniqueAuthArray = [];
+     authArray = data.map(function(article){
+       return article.author
+    })
+    uniqueAuthArray =  $.unique(authArray);
+    return uniqueAuthArray;
+  };
+
+  function arrayOfWords(data){
+    firstWordsArray = [];
+    wordsArray = [];
+    realWordsArray = [];
+    letterArray = [];
+    data.forEach(function(article){
+      firstWordsArray.push(article.body) ;
+    })
+    firstWordsArray.forEach(function(body){
+      wordsArray.push(body.split(' '));
+    })
+    wordsArray.forEach(function(words){
+      words.forEach(function(inWords){
+        realWordsArray.push(inWords);
+      })
+    })
+    return realWordsArray;
+  };
+
+  function wordNum(data){
+    var counter = 0;
+    data.forEach(function(word){
+      counter += word.length;
+    })
+    return counter;
+    }
+
+  function avgWordFunc(data, data2){
+    var avgWord = data/data2;
+    return avgWord.toFixed(2);
+    }
+
+  function avgAuth(){
+    var mainArray = initArray();
+    var authorList = numOfAuths(initArray());
+    var searchResults = [];
+    var authorWords;
+    var authorLetters;
+    var authorAvg;
+
+    authorList.forEach(function(author) {
+        searchResults = mainArray.filter(searchFilter(author, 'author'));
+        authorWords = arrayOfWords(searchResults);
+        authorLetters = wordNum(authorWords)
+        var authorAvg = avgWordFunc(authorLetters, authorWords.length);
+        $('#avgAuth').append("<li>" + author + ": " + authorAvg + "</li>");
+    });
+}
+
+function searchFilter(value, property){
+  var searchFor = value;
+  return function(item) {
+    return item[property] == value;
+  };
+}
+
+
+
+$('#numOfArticles').text(numOfArticles(initArray()));
+console.log('number of articles ' + numOfArticles(initArray()));
+
+$('#numOfAuths').text(numOfAuths(initArray()).length);
+console.log('number of authors ' + numOfAuths(initArray()).length);
+
+$('#numOfWords').text(arrayOfWords(initArray()).length);
+console.log(arrayOfWords(initArray()).length);
+
+$('#avgWordL').text(avgWordFunc(wordNum(arrayOfWords(initArray())), arrayOfWords(initArray()).length))
+console.log(avgWordFunc(wordNum(arrayOfWords(initArray())), arrayOfWords(initArray()).length))
+avgAuth();
+
+  });
--- JS/append.js
+++ JS/append.js
@@ -2,18 +2,20 @@
 var newArt = [];
 var catForm = [];
 var bigArray = [];
+var eTag;
 
 $(function(){
   $.ajax({
      type: "HEAD",
      async: true,
      url: "blogArticles.json",
-     complete: function(XMLHttpRequest, textStatus){
-       var eTag = XMLHttpRequest.getResponseHeader('ETag');
-       console.log(eTag);
-       localStorage.setItem('ergodicEtag', eTag)
+     success: function(data, textStatus, xhr){
+       eTag = xhr.getResponseHeader('ETag');
+       get_json();
      }
    });
+
+   var get_json = function(){
        $.getJSON('blogArticles.json', function(data){
          localStorage.setItem('blogData', JSON.stringify(data));
          var rawBlogData = localStorage.getItem('blogData');
@@ -21,17 +23,63 @@ $(function(){
          for (i = 0; i < blogData.length; i ++){
            bigArray[i] = new Articles(blogData[i]);
           }
+          for (i = 0; i < bigArray.length; i++){
+            if(bigArray[i].markdown){
+              bigArray[i].body = marked(bigArray[i].markdown);
+            }
+          }
           $.get('template.handlebars', function(data){
             for (i = 0; i < bigArray.length; i++) {
               var compTemp = Handlebars.compile(data);
               var handPush = compTemp(bigArray[i]);
               $('.articleBox').append(handPush);
+              localStorage.setItem('Etag', eTag);
+              $('.cateFill').hide();
+              $('.articleBox').find('.bod p:not(:first)').hide();
             }
+            $(function(){
+              $('.readMore').click(function(){
+                var scrollTo = $(this).parent().position().top;
+                $(this).parent().find('.bod p:not(:first)').slideToggle();
+                $("html, body").animate({scrollTop: scrollTo}, 300);
+                  });
+                });
+              });
           })
+        }
+          var authForm = [];
+          var popAform = function(a){
+            $('#auth').append('<option value=' + a + '>' + a + '</option>');
+          };
+
+          var popCform = function(c){
+            $('#cate').append('<option value=' + c +'>' + c + '</option>');
+          };
+
+          var popForm = function(){
+            var tempA = [];
+            var tempC = [];
+            for (i = 0; i < bigArray.length; i ++){
+              tempA[i] =bigArray[i].author;
+              tempC[i] = bigArray[i].category;
+            }
+            authForm = $.unique(tempA)
+            authForm.sort();
+            for (i = 0; i < authForm.length; i++){
+              popAform(authForm[i]);
+            }
+            catForm = $.unique(tempC);
+            catForm.sort();
+            for (i = 0; i < catForm.length; i++) {;
+            popCform(catForm[i]);
+            };
+          };
+          popForm();
         });
 
-$('#Enter').click(function(){
-  window.location = "newentry.html";
+$('#Enter').click(function(e){
+  e.preventDefault();
+  window.location = "admin_stats.html";
 });
 // OBJECT CONSTRUCTOR*************
 function Articles(obj) {
@@ -42,69 +90,12 @@ function Articles(obj) {
    this.publishedOn = obj.publishedOn;
    this.body = obj.body;
    this.getDay = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-
+   this.markdown = obj.markdown;
  };
 
- // var pushArray = function(){
- //    for (i = 0; i < blog.rawData.length; i ++){
- //      newArt[i] = new Articles(blog.rawData[i]);
- //    };
- //    newArt.sort(function(a, b){
- //      a = new Date (a.publishedOn);
- //      b = new Date (b.publishedOn);
- //      return b - a;
- //    });
- //    var stringArray = JSON.stringify(newArt)
- //    localStorage.setItem('articles', stringArray)
- //  }
-
-// IMPORT WITH HANDLEBAR***********
-  // var createAll = function(){
-  //     $.get('template.handlebars', function(data){
-  //       for (i = 0; i < bigArray.length; i++) {
-  //         var compTemp = Handlebars.compile(data);
-  //         var handPush = compTemp(bigArray[i]);
-  //         $('.articleBox').append(compTemp(handPush));
-  //         }
-  //         $('.cateFill').hide();
-  //         $('.bod').find('p:not(:first-child)').hide();
-  //         $('.readMore').click(function(){
-  //           var scrollTo = $(this).parent().position().top;
-  //           $(this).parent().find('.bod p:not(:first)').slideToggle();
-  //           $("html, body").animate({scrollTop: scrollTo}, 300);
-  //         })
-  //       })
-  //     };
-// POPULATING FORM******************
-
-  var authForm = [];
 
 
-  var popAform = function(a){
-    $('#auth').append('<option value=' + a + '>' + a + '</option>');
-  };
 
-  var popCform = function(c){
-    $('#cate').append('<option value=' + c +'>' + c + '</option>');
-  };
-
-  var popForm = function(){
-
-    var tempA = [];
-    for (i = 0; i < bigArray.length; i ++){
-      authForm.push(bigArray[i].author);
-      tempA[i] = bigArray[i].category;
-    }
-    authForm.sort();
-    for (i = 0; i < authForm.length; i++){
-      popAform(authForm[i]);
-    }
-    catForm = $.unique(tempA);
-    catForm.sort();
-    for (i = 0; i < catForm.length; i++) {;
-    popCform(catForm[i]);
-    };
-  };
 
 // FILTERING BY CATEGORY****************
   $('#cate').change(function(){
@@ -129,10 +120,3 @@ function Articles(obj) {
       $(".authFill:contains('" + getAuth + "')").parent().show();
     };
   });
-
-
-// CALLING ALL FUNCTIONS*******************
-
-
-
-});
